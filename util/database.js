6f7130db743c6713548eb54b58a34b31a5083a12
yashojha19 <yashojha19@gmail.com>
2019-02-19 09:16:05 +0530

    init commit

---
+++ util/database.js
@@ -0,0 +1,10 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+
+// module.exports = sequelize;
+
+const mongoose = require("mongoose")
